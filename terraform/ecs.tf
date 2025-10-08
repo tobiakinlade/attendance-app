4830b1217e90b96a7abed305dd793cdfee9df452
“Tobi <tobi.akinlade.co@gmail.com>
2025-10-08 22:04:03 +0100

    ecr image delete option

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -2,6 +2,7 @@
 resource "aws_ecr_repository" "main" {
   name                 = "${var.project_name}-${var.environment}"
   image_tag_mutability = "MUTABLE"
+  force_delete = true
 
   image_scanning_configuration {
     scan_on_push = true
