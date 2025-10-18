2a5be0022466dd2945a9c39b9ee843318d02f779
“Tobi <tobi.akinlade.co@gmail.com>
2025-10-18 19:57:40 +0100

    cloudwatch alarm

---
+++ terraform/cloudwatch-alarm.tf
@@ -0,0 +1,126 @@
+# SNS Topic for Alarms
+resource "aws_sns_topic" "alarms" {
+  name = "${var.project_name}-${var.environment}-alarms"
+}
+
+resource "aws_sns_topic_subscription" "alarm_email" {
+  topic_arn = aws_sns_topic.alarms.arn
+  protocol  = "email"
+  endpoint  = var.alarm_email  # Add this variable
+}
+
+# High CPU Alarm
+resource "aws_cloudwatch_metric_alarm" "ecs_high_cpu" {
+  alarm_name          = "${var.project_name}-${var.environment}-high-cpu"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = "300"
+  statistic           = "Average"
+  threshold           = "80"
+  alarm_description   = "This metric monitors ECS CPU utilization"
+  alarm_actions       = [aws_sns_topic.alarms.arn]
+
+  dimensions = {
+    ClusterName = aws_ecs_cluster.main.name
+    ServiceName = aws_ecs_service.app.name
+  }
+}
+
+# High Memory Alarm
+resource "aws_cloudwatch_metric_alarm" "ecs_high_memory" {
+  alarm_name          = "${var.project_name}-${var.environment}-high-memory"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "MemoryUtilization"
+  namespace           = "AWS/ECS"
+  period              = "300"
+  statistic           = "Average"
+  threshold           = "80"
+  alarm_description   = "This metric monitors ECS memory utilization"
+  alarm_actions       = [aws_sns_topic.alarms.arn]
+
+  dimensions = {
+    ClusterName = aws_ecs_cluster.main.name
+    ServiceName = aws_ecs_service.app.name
+  }
+}
+
+# Target Unhealthy Alarm
+resource "aws_cloudwatch_metric_alarm" "unhealthy_targets" {
+  alarm_name          = "${var.project_name}-${var.environment}-unhealthy-targets"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "UnHealthyHostCount"
+  namespace           = "AWS/ApplicationELB"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "0"
+  alarm_description   = "Alert when targets are unhealthy"
+  alarm_actions       = [aws_sns_topic.alarms.arn]
+
+  dimensions = {
+    TargetGroup  = aws_lb_target_group.app.arn_suffix
+    LoadBalancer = aws_lb.main.arn_suffix
+  }
+}
+
+# High 5XX Error Rate
+resource "aws_cloudwatch_metric_alarm" "high_5xx_errors" {
+  alarm_name          = "${var.project_name}-${var.environment}-high-5xx-errors"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "HTTPCode_Target_5XX_Count"
+  namespace           = "AWS/ApplicationELB"
+  period              = "300"
+  statistic           = "Sum"
+  threshold           = "10"
+  alarm_description   = "Alert on high 5XX error rate"
+  alarm_actions       = [aws_sns_topic.alarms.arn]
+
+  dimensions = {
+    LoadBalancer = aws_lb.main.arn_suffix
+  }
+}
+
+# RDS High CPU
+resource "aws_cloudwatch_metric_alarm" "rds_high_cpu" {
+  alarm_name          = "${var.project_name}-${var.environment}-rds-high-cpu"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/RDS"
+  period              = "300"
+  statistic           = "Average"
+  threshold           = "80"
+  alarm_description   = "RDS CPU is too high"
+  alarm_actions       = [aws_sns_topic.alarms.arn]
+
+  dimensions = {
+    DBInstanceIdentifier = aws_db_instance.main.id
+  }
+}
+
+# RDS Low Storage
+resource "aws_cloudwatch_metric_alarm" "rds_low_storage" {
+  alarm_name          = "${var.project_name}-${var.environment}-rds-low-storage"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "FreeStorageSpace"
+  namespace           = "AWS/RDS"
+  period              = "300"
+  statistic           = "Average"
+  threshold           = "5000000000"  # 5GB in bytes
+  alarm_description   = "RDS storage is running low"
+  alarm_actions       = [aws_sns_topic.alarms.arn]
+
+  dimensions = {
+    DBInstanceIdentifier = aws_db_instance.main.id
+  }
+}
+
+# Output SNS Topic ARN
+output "sns_topic_arn" {
+  value = aws_sns_topic.alarms.arn
+}
